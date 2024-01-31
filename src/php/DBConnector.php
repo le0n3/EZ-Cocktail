b244ecaf27597974aee0b5063ab1f4063c0adc74
Leon Engelhardt <leon@leon-engelhardt.de>
2024-01-31 10:16:15 +0100

    Error Heandling hinzugefügt

--- src/php/DBConnector.php
+++ src/php/DBConnector.php
@@ -3,7 +3,6 @@
 include_once("Zutat/zutat.php");
 class DBConnection
 {
-
     private static $servername = "localhost";
     private static $dbname = "ez_cocktail";
     private static $connection = null;
@@ -26,19 +25,23 @@ class DBConnection
 
     public static function readAll()
     {
-        $ingredienz = array();
-        $conn = self::getConnection();
+        try {
 
-        $sql = "SELECT zutat.id, typ.name as Typen, zutat.name as Zutat, zutat.beschreibung,zutatinventar.menge, einheit.einheitBezeichnung FROM `zutat` Left JOIN einheit on zutat.id = einheit.zutatId Left JOIN typ on zutat.id = typ.zutatId left Join zutatinventar on zutat.id = zutatinventar.zutatId; ";
-        $result = $conn->query($sql);
+            $ingredienz = array();
+            $conn = self::getConnection();
 
-        if ($result->num_rows > 0) {
+            $sql = "SELECT zutat.id, typ.name as Typen, zutat.name as Zutat, zutat.beschreibung,zutatinventar.menge, einheit.einheitBezeichnung FROM `zutat` Left JOIN einheit on zutat.id = einheit.zutatId Left JOIN typ on zutat.id = typ.zutatId left Join zutatinventar on zutat.id = zutatinventar.zutatId; ";
+            $result = $conn->query($sql);
 
-            while ($row = $result->fetch_assoc()) {
-                array_push($ingredienz, new Ingredient($row["id"], $row["Zutat"], $row["beschreibung"], $row["menge"], $row["Typen"], $row["einheitBezeichnung"]));
+            if ($result->num_rows > 0) {
+
+                while ($row = $result->fetch_assoc()) {
+                    array_push($ingredienz, new Ingredient($row["id"], $row["Zutat"], $row["beschreibung"], $row["menge"], $row["Typen"], $row["einheitBezeichnung"]));
+                }
             }
-        } else {
-            echo "0 results";
+
+        }catch (Exception $e){
+            return $ingredienz;
         }
         return $ingredienz;
 //$conn->close();
