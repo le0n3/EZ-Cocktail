c281dae56d1db6ded8e3c2219f39659e3b52b6fb
Andrej <andrej.weber1@berufsschulebt1.onmicrosoft.com>
2024-02-05 08:11:14 +0100

    Korrigiert form submit

--- src/html/ingredientModal.php
+++ src/html/ingredientModal.php
@@ -8,45 +8,47 @@ $ingredeant = null;
 ?>
 
 
-<div class="modal-header">
-    <h5 class="modal-title" id="modalTitle">Zutat hinzufügen</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-</div>
-<div class="modal-body">
-    <div class="container">
-        <div class="row mb-2">
-            <div class="col">
-                <label for="Zutat" class="form-label">Zutat</label>
-                <input type="text" class="form-control" id="Zutat" name="Zutat" value="<?php echo !is_null($ingredeant) ? $ingredeant->getIngredient(): ''; ?>">
+<form action="../php/ManipulteIngredients.php" autocomplete="off" method="post">
+    <div class="modal-header">
+        <h5 class="modal-title" id="modalTitle">Zutat hinzufügen</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        <div class="container">
+            <div class="row mb-2">
+                <div class="col">
+                    <label for="Zutat" class="form-label">Zutat</label>
+                    <input type="text" class="form-control" id="Zutat" name="Zutat" value="<?php echo !is_null($ingredeant) ? $ingredeant->getIngredient(): ''; ?>">
+                </div>
             </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col">
-                <label for="Menge" class="form-label">Menge</label>
-                <input type="text" class="form-control" id="Menge" name="Menge" value="<?php echo !is_null($ingredeant) ? $ingredeant->getQuantity(): ''; ?>">
+            <div class="row mb-2">
+                <div class="col">
+                    <label for="Menge" class="form-label">Menge</label>
+                    <input type="text" class="form-control" id="Menge" name="Menge" value="<?php echo !is_null($ingredeant) ? $ingredeant->getQuantity(): ''; ?>">
+                </div>
+                <div class="col">
+                    <label for="EinheitKürzel" class="form-label">Einheit Kürzel</label>
+                    <input type="text" class="form-control" id="EinheitKürzel" name="EinheitKürzel">
+                </div>
+                <div class="col">
+                    <label for="Einheit" class="form-label">Einheit</label>
+                    <input type="text" class="form-control" id="Einheit" name="Einheit" value="<?php echo !is_null($ingredeant) ? $ingredeant->getUnit(): ''; ?>">
+                </div>
             </div>
-            <div class="col">
-                <label for="EinheitKürzel" class="form-label">Einheit Kürzel</label>
-                <input type="text" class="form-control" id="EinheitKürzel" name="EinheitKürzel">
+            <div class="row mb-2">
+                <div class="col">
+                    <label for="Typ" class="form-label">Typ</label>
+                    <input type="text" class="form-control" id="Typ" name="Typ" value="<?php echo !is_null($ingredeant) ? $ingredeant->getType(): ''; ?>">
+                </div>
             </div>
-            <div class="col">
-                <label for="Einheit" class="form-label">Einheit</label>
-                <input type="text" class="form-control" id="Einheit" name="Einheit" value="<?php echo !is_null($ingredeant) ? $ingredeant->getUnit(): ''; ?>">
+            <div class="row mb-2">
+                <label for="Beschreibung" class="form-label">Beschreibung</label>
+                <textarea class="form-control ms-2" id="Beschreibung" name="Beschreibung" rows="3" style="width: 96%;"> <?php echo !is_null($ingredeant) ? $ingredeant->getDescription(): ''; ?></textarea>
             </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col">
-                <label for="Typ" class="form-label">Typ</label>
-                <input type="text" class="form-control" id="Typ" name="Typ" value="<?php echo !is_null($ingredeant) ? $ingredeant->getType(): ''; ?>">
-            </div>
-        </div>
-        <div class="row mb-2">
-            <label for="Beschreibung" class="form-label">Beschreibung</label>
-            <textarea class="form-control ms-2" id="Beschreibung" rows="3" style="width: 96%;"> <?php echo !is_null($ingredeant) ? $ingredeant->getDescription(): ''; ?></textarea>
         </div>
     </div>
-</div>
-<div class="modal-footer">
-    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
-    <button type="button" class="btn btn-primary">Speichern</button>
-</div>
+    <div class="modal-footer">
+        <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
+        <button type="submit" class="btn btn-primary">Speichern</button>
+    </div>
+</form>
